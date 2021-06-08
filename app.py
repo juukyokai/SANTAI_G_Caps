a22b97f478c5667295779012e2ae99a51cd1f16b
Juukyo <juukyokai00@gmail.com>
2021-06-08 19:47:14 +0700

    adding fixxx configuration for ML in flask webapp

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import werkzeug
-from keras.preprocessing.image import ImageDataGenerator, image
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 import numpy as np
 import os
